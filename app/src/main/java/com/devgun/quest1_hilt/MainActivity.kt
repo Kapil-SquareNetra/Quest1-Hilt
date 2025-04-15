f72789e8712128566f2e3bef9478cc3191a66186
kapil <kapleshwar.7925@gmail.com>
2025-04-15 12:52:43 +0530

    Navigation - WIP

--- app/src/main/java/com/devgun/quest1_hilt/MainActivity.kt
+++ app/src/main/java/com/devgun/quest1_hilt/MainActivity.kt
@@ -2,18 +2,17 @@ package com.devgun.quest1_hilt
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private lateinit var navController: NavController
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        enableEdgeToEdge()
+        setContent {
 
-
-        navController = findNavController(R.id.nav_host_fragment_activity_main)
-
+        }
     }
 }
